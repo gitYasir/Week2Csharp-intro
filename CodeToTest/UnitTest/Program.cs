3278c76326a3457f29380dac16cedb59fd06eaed
Yasir <yasir.ibrahim@hotmail.co.uk>
2022-11-15 15:07:01 +0000

    refactored task code and wrote tests

--- CodeToTest/UnitTest/Program.cs
+++ CodeToTest/UnitTest/Program.cs
@@ -20,5 +20,25 @@
                 }
             return greeting;
             }
+
+        public static string AvailableClassifications( int ageOfViewer ) {
+            string result;
+            if ( ageOfViewer >= 4 && ageOfViewer <= 11 ) {
+                result = "U & PG films are available.";
+                }
+            else if ( ageOfViewer >= 12 && ageOfViewer <= 14 ) {
+                result = "U, PG & 12 films are available.";
+                }
+            else if ( ageOfViewer >= 15 && ageOfViewer <= 17 ) {
+                result = "U, PG, 12 & 15 films are available.";
+                }
+            else if ( ageOfViewer >= 18 ) {
+                result = "All films are available.";
+                }
+            else {
+                result = "Enter age 4 and over";
+                }
+            return result;
+            }
         }
     }
---
+++ CodeToTest/UnitTestingCode/MovieClassificationTests.cs
@@ -0,0 +1,36 @@
+﻿using CodeToTest;
+
+namespace UnitTestingCode {
+    internal class MovieClassificationTests {
+
+        [TestCase( 3 )]
+        [TestCase( -1 )]
+        public void GivenAgeOfViewerUnder4( int age ) {
+            Assert.That( "Enter age 4 and over", Is.EqualTo( Program.AvailableClassifications( age ) ) );
+            }
+
+        [TestCase( 4 )]
+        [TestCase( 11 )]
+        public void GivenAgeOfViewerBetween4And11Inc( int age ) {
+            Assert.That( "U & PG films are available.", Is.EqualTo( Program.AvailableClassifications( age ) ) );
+            }
+
+        [TestCase( 12 )]
+        [TestCase( 14 )]
+        public void GivenAgeOfViewerBetween12To14Inc( int age ) {
+            Assert.That( "U, PG & 12 films are available.", Is.EqualTo( Program.AvailableClassifications( age ) ) );
+            }
+
+        [TestCase( 15 )]
+        [TestCase( 17 )]
+        public void GivenAgeOfViewerBetween15To17Inc( int age ) {
+            Assert.That( "U, PG, 12 & 15 films are available.", Is.EqualTo( Program.AvailableClassifications( age ) ) );
+            }
+
+        [TestCase( 18 )]
+        [TestCase( 58 )]
+        public void GivenAgeOfViewer18AndOver( int age ) {
+            Assert.That( "All films are available.", Is.EqualTo( Program.AvailableClassifications( age ) ) );
+            }
+        }
+    }
