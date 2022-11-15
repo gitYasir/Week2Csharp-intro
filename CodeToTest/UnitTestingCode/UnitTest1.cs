347c7bd78a262d07d8cafd9db86f8b17e1f50dc3
Yasir <yasir.ibrahim@hotmail.co.uk>
2022-11-15 11:24:53 +0000

    refactored code and wrote unit test

---
+++ CodeToTest/UnitTestingCode/UnitTest1.cs
@@ -0,0 +1,13 @@
+using CodeToTest;
+
+namespace UnitTests {
+    public class GreetingTests {
+        [Test]
+        public void GivenATimeOf21_Greeting_ReturnsGoodEvening() {
+            var time = 21;
+            var expectedGreeting = "Good evening!";
+            var result = Program.Greeting( time );
+            Assert.That( result, Is.EqualTo( expectedGreeting ) );
+            }
+        }
+    }
