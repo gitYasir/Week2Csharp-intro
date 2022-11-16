952d16ddbf6d964afa7d1b523c082b1cce21d962
Yasir <yasir.ibrahim@hotmail.co.uk>
2022-11-16 16:39:35 +0000

    Average method written and tests passing

--- Operators_ControlFlow_Lab_Starter/Op_CtrlFlow_Lib/Exercises.cs
+++ Operators_ControlFlow_Lab_Starter/Op_CtrlFlow_Lib/Exercises.cs
@@ -1,19 +1,20 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace Op_CtrlFlow
-{
-    public class Exercises
-    {
-        public static bool MyMethod(int num1, int num2)
-        {
-            return num1 == num2 ? false : (num1 % num2) == 0;
-        }
+namespace Op_CtrlFlow {
+    public class Exercises {
+        public static bool MyMethod( int num1, int num2 ) {
+            return num1 == num2 ? false : ( num1 % num2 ) == 0;
+            }
 
         // returns the average of the array nums
-        public static double Average(List<int> nums)
-        {
-            return -1;
-        }
+        public static double Average( List<int> nums ) {
+            double length = nums.Count;
+
+            double sum = nums.Sum() / length;
+            return nums.Count != 0 ? Math.Round( sum, 1 ) : 0;
+            }
 
         // returns the type of ticket a customer is eligible for based on their age
         // "Standard" if they are between 18 and 59 inclusive
@@ -21,21 +22,18 @@ namespace Op_CtrlFlow
         // "Student" if they are 13-17 inclusive
         // "Child" if they are 5-12
         // "Free" if they are under 5
-        public static string TicketType(int age)
-        {
+        public static string TicketType( int age ) {
             string ticketType = string.Empty;
             return ticketType;
-        }
+            }
 
-        public static string Grade(int mark)
-        {
+        public static string Grade( int mark ) {
             var grade = "";
             return grade;
-        }
+            }
 
-        public static int GetScottishMaxWeddingNumbers(int covidLevel)
-        {
+        public static int GetScottishMaxWeddingNumbers( int covidLevel ) {
             return 0;
+            }
         }
     }
-}
