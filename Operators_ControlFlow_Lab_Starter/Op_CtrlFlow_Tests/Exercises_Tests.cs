b00ce91c0545f16e45b58a8441d53070ec9fb488
Yasir <yasir.ibrahim@hotmail.co.uk>
2022-11-16 16:15:14 +0000

    MyMethod tests written and passing

---
+++ Operators_ControlFlow_Lab_Starter/Op_CtrlFlow_Tests/Exercises_Tests.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using Op_CtrlFlow;
+using System.Collections.Generic;
+
+namespace Op_CtrlFlow_Tests {
+    public class Exercises_Tests {
+        // write unit test(s) for MyMethod here
+        [TestCase( 2, 2 )]
+        [TestCase( 15, 2 )]
+        public void Given2Nums_NotEqualNotModulos0_MyMethod_ShouldReturnFalse( int num1, int num2 ) {
+            Assert.That( Exercises.MyMethod( num1, num2 ), Is.EqualTo( false ) );
+            }
+
+        [TestCase( 10, 2 )]
+        [TestCase( 15, 5 )]
+        public void Given2Nums_NotEqualButModulos0_MyMethod_ShouldReturnTrue( int num1, int num2 ) {
+            Assert.That( Exercises.MyMethod( num1, num2 ), Is.EqualTo( true ) );
+            }
+
+        [Test]
+        public void Average_ReturnsCorrectAverage() {
+            var myList = new List<int>() { 3, 8, 1, 7, 3 };
+            Assert.That( Exercises.Average( myList ), Is.EqualTo( 4.4 ) );
+            }
+
+        [Test]
+        public void WhenListIsEmpty_Average_ReturnsZero() {
+            var myList = new List<int>() { };
+            Assert.That( Exercises.Average( myList ), Is.EqualTo( 0 ) );
+            }
+
+        [TestCase( 100, "OAP" )]
+        [TestCase( 60, "OAP" )]
+        [TestCase( 59, "Standard" )]
+        [TestCase( 18, "Standard" )]
+        [TestCase( 17, "Student" )]
+        [TestCase( 13, "Student" )]
+        [TestCase( 12, "Child" )]
+        [TestCase( 5, "Child" )]
+        [TestCase( 4, "Free" )]
+        [TestCase( 0, "Free" )]
+        public void TicketTypeTest( int age, string expected ) {
+            var result = Exercises.TicketType( age );
+            Assert.That( result, Is.EqualTo( expected ) );
+            }
+        }
+    }
