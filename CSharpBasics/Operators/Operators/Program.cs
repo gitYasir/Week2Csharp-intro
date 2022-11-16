cce1e5d8e3c879018f8538954b4c3cb01969989a
Yasir <yasir.ibrahim@hotmail.co.uk>
2022-11-16 11:29:45 +0000

    practiced integer divisions

--- CSharpBasics/Operators/Operators/Program.cs
+++ CSharpBasics/Operators/Operators/Program.cs
@@ -1,18 +1,33 @@
-﻿#region
-int x = 5;
-int y = 5;
+﻿#region post and prefix
 
-int postfix = x++;
-int prefix = ++y;
+//int x = 5;
+//int y = 5;
 
-Console.WriteLine( $"postfix: {postfix} -- prefix: {prefix}" );
+//int postfix = x++;
+//int prefix = ++y;
 
-/*
-* The for loop shows that it makes no difference if you use a prefix or postfix
-* increment or decrement if they are standalone
-*/
-for ( int i = 0; i < 5; i++ ) {
-    Console.WriteLine( i );
-    }
+//Console.WriteLine( $"postfix: {postfix} -- prefix: {prefix}" );
+
+///*
+//* The for loop shows that it makes no difference if you use a prefix or postfix
+//* increment or decrement if they are standalone
+//*/
+//for ( int i = 0; i < 5; i++ ) {
+//    Console.WriteLine( i );
+//    }
+
 #endregion
+#region integer devision
+//var c = 5 / 2;    // two integers will result in an integer, the remainder is ignored
+//var d = 5.0 / 2;  // where one is a double, the other will be converted to a double
+//var e = 5 / 3;
 
+
+
+//Console.WriteLine( $"c == {c} : d == {d} : e == {e}" );
+
+//double f = 5 / 2;
+
+//Console.WriteLine( f );
+
+#endregion
