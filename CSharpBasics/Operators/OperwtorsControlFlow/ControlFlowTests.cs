eae8165aab98e8c8012d33fc1dfad976de55d385
Yasir <yasir.ibrahim@hotmail.co.uk>
2022-11-16 15:00:06 +0000

    tests passing for pre lunch task

--- CSharpBasics/Operators/OperwtorsControlFlow/ControlFlowTests.cs
+++ CSharpBasics/Operators/OperwtorsControlFlow/ControlFlowTests.cs
@@ -6,5 +6,11 @@
             string expected = "Pass";
             Assert.That( expected, Is.EqualTo( grade ) );
             }
+        [TestCase( 64 )]
+        public void GivenGrade_Checkgrade_ReturnsFail( int mark ) {
+            string grade = CheckGrade.GetGrade( mark );
+            string expected = "Fail";
+            Assert.That( expected, Is.EqualTo( grade ) );
+            }
         }
     }
--- CSharpBasics/Operators/OperwtorsControlFlow/StoneTests.cs
+++ CSharpBasics/Operators/OperwtorsControlFlow/StoneTests.cs
@@ -6,5 +6,9 @@ namespace OperwtorsControlFlow {
         public void GivenPounds_GetStones_ReturnStones( int pounds ) {
             Assert.That( 1, Is.EqualTo( Methods.GetStones( pounds ) ) );
             }
+        [TestCase( 156 )]
+        public void GivenPounds_GetPounds_Returnpounds( int pounds ) {
+            Assert.That( 2, Is.EqualTo( Methods.GetPounds( pounds ) ) );
+            }
         }
     }
