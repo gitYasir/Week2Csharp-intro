9194a26f51b4459d7c277f929db5a192dde5b173
Yasir <yasir.ibrahim@hotmail.co.uk>
2022-11-16 11:01:34 +0000

    practiced increments

--- CSharpBasics/Operators/Operators/Program.cs
+++ CSharpBasics/Operators/Operators/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿#region
+int x = 5;
+int y = 5;
+
+int postfix = x++;
+int prefix = ++y;
+
+Console.WriteLine( $"postfix: {postfix} -- prefix: {prefix}" );
+
+/*
+* The for loop shows that it makes no difference if you use a prefix or postfix
+* increment or decrement if they are standalone
+*/
+for ( int i = 0; i < 5; i++ ) {
+    Console.WriteLine( i );
+    }
+#endregion
+
