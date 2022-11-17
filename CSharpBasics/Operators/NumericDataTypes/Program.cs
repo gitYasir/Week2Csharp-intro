dca29bea1758f56be881c1959d644e7c387f9e0c
Yasir <yasir.ibrahim@hotmail.co.uk>
2022-11-17 19:59:41 +0000

    learened more about var

--- CSharpBasics/Operators/NumericDataTypes/Program.cs
+++ CSharpBasics/Operators/NumericDataTypes/Program.cs
@@ -1,7 +1,13 @@
-﻿namespace NumericDataTypes {
+﻿namespace NumericDataTypesApp {
     internal class Program {
         static void Main( string[] args ) {
-            Console.WriteLine( "Hello, World!" );
+            int x = 3; // explicitly typed
+            var y = 2; // implicitly typed
+
+            var name = "Yasir";
+            var isClean = true;
+            var letter = "A";
+            var uLongNum = 52uL;
             }
         }
     }
