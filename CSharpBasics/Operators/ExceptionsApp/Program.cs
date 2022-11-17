81161a752643bc8deea1b5bc1b87382d932c74a3
Yasir <yasir.ibrahim@hotmail.co.uk>
2022-11-17 12:02:20 +0000

    practiced try catch and finally

---
+++ CSharpBasics/Operators/ExceptionsApp/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ExceptionsApp {
+    internal class Program {
+        static void Main( string[] args ) {
+            string text;
+            string fileName = "HelloWorld.txt";
+            try {
+                text = File.ReadAllText( fileName );
+                Console.WriteLine( text );
+                }
+            catch ( FileNotFoundException e ) {
+                Console.WriteLine( $"Sorry {fileName} could not be found" );
+                }
+            catch ( Exception e ) {
+                Console.WriteLine( "General handle for all exceptions" );
+                }
+            finally {
+                Console.WriteLine( "We made it here!" );
+                }
+            }
+        }
+    }
---
+++ Operators_ControlFlow_Lab_Starter/LoopsApp/LoopTypes.cs
@@ -0,0 +1,53 @@
+﻿namespace LoopsApp {
+    internal class LoopTypes {
+        internal static int HighestForEachLoop( List<int> nums ) {
+            int highest = nums[ 0 ];
+            // or int highest = int.MinValue;
+            foreach ( int x in nums ) {
+                if ( x > highest ) {
+                    highest = x;
+                    }
+                }
+            return highest;
+            }
+
+        internal static int HighestForLoop( List<int> nums ) {
+            int highest = nums[ 0 ];
+            // or int highest = int.MinValue;
+            for ( int i = 0; i < 5; i++ ) {
+                if ( nums[ i ] > highest ) {
+                    highest = nums[ i ];
+                    }
+                }
+            return highest;
+            }
+
+        internal static int HighestWhileLoop( List<int> nums ) {
+            int highest = nums[ 0 ];
+            // or int highest = int.MinValue;
+            int i = 0;
+            while ( i < nums.Count ) {
+                if ( nums[ i ] > highest ) {
+                    highest = nums[ i ];
+                    }
+                i++;
+                }
+            return highest;
+            }
+
+        internal static int HighestDoWhileLoop( List<int> nums ) {
+            int highest = nums[ 0 ];
+            // or int highest = int.MinValue;
+            int i = 0;
+            do {
+                if ( nums[ i ] > highest ) {
+                    highest = nums[ i ];
+                    }
+                i++;
+                } while ( i < nums.Count );
+            return highest;
+            }
+
+        }
+
+    }
