d267bfd5287efac290c5c2f93841bc71ee5da7c1
Yasir <yasir.ibrahim@hotmail.co.uk>
2022-11-18 01:31:57 +0000

    calculator methods written and all tests passing

--- DataTypes_Lab_Starter/DataTypes_Lib/IntegerCalc.cs
+++ DataTypes_Lab_Starter/DataTypes_Lib/IntegerCalc.cs
@@ -1,32 +1,37 @@
 ﻿using System;
 
-namespace DataTypes_Lib
-{
-    public static class IntegerCalc
-    {
-        public static int Add(int num1, int num2)
-        {
-             throw new NotImplementedException();
-        }
+namespace DataTypes_Lib {
+    public static class IntegerCalc {
+        public static int Add( int num1, int num2 ) {
+            int x = num1 + num2;
 
-        public static int Subtract(int num1, int num2)
-        {
-             throw new NotImplementedException();
-        }
+            return Convert.ToInt16( x );
+            }
 
-        public static int Multiply(int num1, int num2)
-        {
-            throw new NotImplementedException();
-        }
+        public static int Subtract( int num1, int num2 ) {
+            int num3 = num1 - num2;
+            return Convert.ToInt16( num3 );
+            }
 
-        public static int Divide(int num1, int num2)
-        {
-            throw new NotImplementedException();
-        }
+        public static int Multiply( int num1, int num2 ) {
+            int y = num1 * num2;
+            return Convert.ToInt32( y );
+            }
 
-        public static int Modulus(int num1, int num2)
-        {
-            throw new NotImplementedException();
+        public static int Divide( int num1, int num2 ) {
+            if ( num2 == 0 ) {
+                throw new ArgumentException( "Can't divide by zero" );
+                }
+            int z = num1 / num2;
+            return Convert.ToInt16( z );
+            }
+
+        public static int Modulus( int num1, int num2 ) {
+            if ( num2 == 0 ) {
+                throw new ArgumentException( "Can't modulo by zero" );
+                }
+            int a = num1 % num2;
+            return Convert.ToInt16( a );
+            }
         }
     }
-}
