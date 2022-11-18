db657d5e664ec134e251e475599384a3c82367dc
Yasir <yasir.ibrahim@hotmail.co.uk>
2022-11-18 10:59:26 +0000

    start strings lesson

---
+++ DataTypeAndMethods/MoreDataTypes/Program.cs
@@ -0,0 +1,7 @@
+﻿namespace MoreDataTypes {
+    internal class Program {
+        static void Main( string[] args ) {
+            Console.WriteLine( "Hello, World!" );
+            }
+        }
+    }
