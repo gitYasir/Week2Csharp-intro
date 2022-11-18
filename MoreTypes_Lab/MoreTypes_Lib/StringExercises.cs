d6efc4ad62e5f59d20c54e2d447bfcb161716ddc
Yasir <yasir.ibrahim@hotmail.co.uk>
2022-11-18 14:49:12 +0000

    starting more data types labs

---
+++ MoreTypes_Lab/MoreTypes_Lib/StringExercises.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace MoreTypes_Lib
+{
+    public class StringExercises
+    {
+        // manipulates and returns a string - see the unit test for requirements
+        public static string ManipulateString(string input, int num)
+        {
+            throw new NotImplementedException();
+        }
+
+        // returns a formatted address string given its components
+        public static string Address(int number, string street, string city, string postcode)
+        {
+            throw new NotImplementedException();
+        }
+        // returns a string representing a test score, written as percentage to 1 decimal place
+        public static string Scorer(int score, int outOf)
+        {
+            throw new NotImplementedException();
+        }
+
+        // returns the double represented by the string, or -999 if conversion is not possible
+        public static double ParseNum(string numString)
+        {
+            throw new NotImplementedException();
+        }
+
+        // Returns the a string containing the count of As, Bs, Cs and Ds in the parameter string
+        // all other letters are ignored
+        public static string CountLetters(string input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
