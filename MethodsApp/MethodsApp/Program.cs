0e43f4603fd951db86cdcd046b0da2e2220b6b44
Yasir <yasir.ibrahim@hotmail.co.uk>
2022-11-21 20:25:55 +0000

    undid changes to methods app

--- MethodsApp/MethodsApp/Program.cs
+++ MethodsApp/MethodsApp/Program.cs
@@ -24,58 +24,41 @@
             //var janeDoe = (firstName, lastName, age);
             //Console.WriteLine(janeDoe);
 
-            //        var maths = MultiReturn( 10, 5 );
-            //        Console.WriteLine( maths );
-            //        Console.WriteLine( $"Multiple is {maths.multiply}" );
-            //        }
-
-            //    public static int DoThis( int x, string y = "happy", char c = 'x' ) {
-            //        Console.WriteLine( $"I'm feeling {y} {c}" );
-            //        return x * x;
-            //        }
-            //    public static string OrderPizza( bool anchovies, bool pineapple, bool mushrooms = false, string x = "" ) {
-            //        var pizza = "Pizza with tomato sauce, cheese, ";
-            //        string custom = x;
-            //        if ( anchovies )
-            //            pizza += "anchovies, ";
-            //        if ( pineapple )
-            //            pizza += "pineapple, ";
-            //        if ( mushrooms )
-            //            pizza += "mushrooms, ";
-            //        if ( custom.Length > 0 ) {
-            //            pizza = $"{pizza}{x}.";
-            //            }
-            //        Console.WriteLine( pizza );
-            //        return pizza;
-            //        }
-
-            //    public static (int multiply, int divide) MultiReturn( int x, int y ) {
-            //        int multiply = x * y;
-            //        int divide = x / y;
-
-            //        return (multiply, divide);
-            //        }
-
-            //    }
-            //}
-            //Test();
-            //static void Test()
-            //{
-            //    Random x = new Random( 5 );
-            //    int y = x.Next( 1, 7 );
-            //    int z = x.Next( 1, 7 );
-            //    Console.WriteLine( y );
-            //    Console.WriteLine( z );
-            //}
-            Random rng = new Random( 5 );
-            RollDice( rng );
-            static int RollDice( Random rng )
-            {
-                var num1 = rng.Next( 1, 7 );
-                var num2 = rng.Next( 1, 7 );
-                Console.WriteLine( num1 + num2 );
-                return num1 + num2;
-            }
+            var maths = MultiReturn( 10, 5 );
+            Console.WriteLine( maths );
+            Console.WriteLine( $"Multiple is {maths.multiply}" );
         }
+
+        public static int DoThis( int x, string y = "happy", char c = 'x' )
+        {
+            Console.WriteLine( $"I'm feeling {y} {c}" );
+            return x * x;
+        }
+        public static string OrderPizza( bool anchovies, bool pineapple, bool mushrooms = false, string x = "" )
+        {
+            var pizza = "Pizza with tomato sauce, cheese, ";
+            string custom = x;
+            if ( anchovies )
+                pizza += "anchovies, ";
+            if ( pineapple )
+                pizza += "pineapple, ";
+            if ( mushrooms )
+                pizza += "mushrooms, ";
+            if ( custom.Length > 0 )
+            {
+                pizza = $"{pizza}{x}.";
+            }
+            Console.WriteLine( pizza );
+            return pizza;
+        }
+
+        public static (int multiply, int divide) MultiReturn( int x, int y )
+        {
+            int multiply = x * y;
+            int divide = x / y;
+
+            return (multiply, divide);
+        }
+
     }
 }
